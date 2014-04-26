918c65cfe8b6d8c76426b49048cbe8dfe6b14e34
emilieCBL <emiliecoulombel@gmail.com>
2014-04-27 01:10:03 +0200

    Update PlaySound.java

--- src/tetraword/PlaySound.java
+++ src/tetraword/PlaySound.java
@@ -2,13 +2,14 @@ package tetraword;
 
 import java.io.File;
 import java.io.IOException;
- 
- import javax.sound.sampled.AudioFormat;
+
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
  
  
  public class PlaySound {    
@@ -18,7 +19,7 @@ import javax.sound.sampled.SourceDataLine;
      public static SourceDataLine line = null;
      private AudioFormat audioFormat = null;
  
-     public PlaySound(File f) throws Exception{
+     public PlaySound(File f) throws UnsupportedAudioFileException, IOException{
          audioStream = AudioSystem.getAudioInputStream(f);
          audioFormat = audioStream.getFormat();
          DataLine.Info info = new DataLine.Info(SourceDataLine.class,audioFormat);
@@ -63,56 +64,21 @@ import javax.sound.sampled.SourceDataLine;
      }
  
      
-   /*  public static void Playlist() throws Exception{
-    	 
- 	    String Nom = null ;
- 	    
- 	    String tabMusique[]= new String [5];
- 	    PlaySound wp;
- 	   
- 	    tabMusique[0]= "bin/tetraword/audio/testi.wav" ;
- 	    tabMusique[1]= "bin/tetraword/audio/Tambourin.wav" ;
- 	    tabMusique[2]= "bin/tetraword/audio/testi.wav" ;
- 	    tabMusique[3]= "bin/tetraword/audio/testi.wav" ;
- 	    tabMusique[4]= "bin/tetraword/audio/testi.wav" ;
- 	   
- 	   int  i = 0 , j =0;
- 	       
- 	    while (i!=100){	     	   
- 	           wp = new PlaySound(new File(tabMusique[i]));
- 	           i++ ;    	   
- 	   
- 	       if(i==5) i=0 ; // RAZ compteur playtist
- 	   
- 	           wp.open();
- 	           System.out.println("zik open");
- 	           wp.play();
- 	           System.out.println("zik play / i ="+i);
- 	   
- 	       while(ThreadPlaySound.endMusic == false) 
- 	         {
- 	    	   
- 	    	//   Thread.sleep(1000); 	
- 	         } 
- 	   
- 	       ThreadPlaySound.endMusic = false ;
- 	   
- 	           //Thread.sleep(1000); 	        	 
- 	   
- 	           wp.stop();
- 	           System.out.println("zik stop");
- 	           wp.close();
- 	           System.out.println("zik close");
- 	     }
-     }
-     */
  
      public static void main(String [] args) throws Exception{
-	    	 
-	          PlaySound wp = new PlaySound(new File("bin/tetraword/audio/testi.wav"));
+	    	int i =0; 
+	          PlaySound wp = new PlaySound(new File("bin/tetraword/audio/test.wav"));
 	          wp.open();
 	          wp.play();
+    	 while(i==0){
 
+	          if(ThreadPlaySound.endMusic){
+	        	  wp = new PlaySound(new File("bin/tetraword/audio/test.wav"));
+	        	  wp.open();
+	        	  wp.play();
+	        	  ThreadPlaySound.endMusic = false;
+	          }
+    	 }
 	          
 	          /*
 	             wp.stop();
